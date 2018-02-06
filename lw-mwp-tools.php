ba4bcbcf246669ab31ca6352f5903174d399ad68
Francis Smith <fs1995@ymail.com>
2018-02-05 22:12:42 -0500

    remove unused code

--- lw-mwp-tools.php
+++ lw-mwp-tools.php
@@ -28,7 +28,6 @@ function lw_mwp_tools_menu(){
   add_submenu_page ('lw-mwp-tools', 'PHP error log', 'PHP error log', 'manage_options', 'lw-mwp-tools-php', 'lw_mwp_tools_php');
   add_submenu_page ('lw-mwp-tools', 'NGINX access log', 'NGINX access log', 'manage_options', 'lw-mwp-tools-nginx-access', 'lw_mwp_tools_nginx_access');
   add_submenu_page ('lw-mwp-tools', 'NGINX error log', 'NGINX error log', 'manage_options', 'lw-mwp-tools-nginx-error', 'lw_mwp_tools_nginx_error');
-  //add_submenu_page ('null', 'NGINX log', '', 'manage_options', 'lw-mwp-tools-nginx', 'lw_mwp_tools_nginx');
 }
 
 function lw_mwp_tools_init(){
@@ -37,7 +36,7 @@ function lw_mwp_tools_init(){
 
 function lw_mwp_tools_php(){
   $lw_mwp_tools_log = file_get_contents('/var/log/' . get_current_user() . '-php-fpm-errors.log') or exit("Unable to access PHP error log. Please report this <a href=\"https://wordpress.org/support/plugin/lw-mwp-tools\" target=\"_blank\">here</a>.");
-  echo "<h2>PHP Error Log viewer</h2>This page does not automatically update, you will need to refresh it. If you are troubleshooting WordPress code, have you turned on <a href=\"https://codex.wordpress.org/Debugging_in_WordPress\" target=\"_blank\">WP_DEBUG</a> in wp-config.php?<pre>" . $lw_mwp_tools_log . "</pre>"; //NGINX access and error logs can be found <a href=\"" . admin_url('admin.php?page=lw-mwp-tools-nginx') . "\">in the Managed WordPress control panel</a>.
+  echo "<h2>PHP Error Log viewer</h2>This page does not automatically update, you will need to refresh it. If you are troubleshooting WordPress code, have you turned on <a href=\"https://codex.wordpress.org/Debugging_in_WordPress\" target=\"_blank\">WP_DEBUG</a> in wp-config.php?<pre>" . $lw_mwp_tools_log . "</pre>";
 }
 
 function lw_mwp_tools_nginx_access(){
@@ -50,6 +49,3 @@ function lw_mwp_tools_nginx_error(){
   echo "<h2>NGINX Error Log viewer</h2>This page does not automatically update, you will need to refresh it.<pre>" . $lw_mwp_tools_log . "</pre>";
 }
 
-/*function lw_mwp_tools_nginx(){
-  echo "<h2>NGINX logs can be found here:</h2><img src=\"" . plugins_url('nginx_logs.png', __FILE__ ) . "\" height=\"617\" width=\"1027\">";
-}*/
